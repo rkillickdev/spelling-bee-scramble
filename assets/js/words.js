08a84b03a405755b2df75324a56c4a0cc14638c3
Rob Killick <robkillick@me.com>
2023-04-26 16:18:07 +0000

    Move wordCollection array to separate words js file

---
+++ assets/js/words.js
@@ -0,0 +1,50 @@
+const wordCollection = [
+    {
+        word: "pear",
+        hint: "soft juicy fruit",
+        picture: "assets/images/pear-icon.png",
+        description: "a green pear",
+        level: "easy",
+        category: "fruit"
+    },
+    {
+        word: "kiwi",
+        hint: "New Zealander",
+        picture: "assets/images/kiwi-food-icon.png",
+        description: "a kiwi fruit",
+        level: "easy",
+        category: "fruit"     
+    },
+    {
+        word: "peach",
+        hint: "a furry fruit",
+        picture: "assets/images/peach-fruit-icon.png",
+        description: "a peach",
+        level: "medium",
+        category: "fruit"     
+    },
+    {
+        word: "APPLE",
+        hint: "crunchy fruit",
+        picture: "assets/images/fresh-apple-icon.png",
+        description: "an apple",
+        level: "medium",
+        category: "fruit" 
+    },
+    {
+        word: "carrot",
+        hint: "orange vegetable",
+        picture: "assets/images/carrot-icon.png",
+        description: "a carrot",
+        level: "hard",
+        category: "vegetable" 
+    },
+    {
+        word: "cherry",
+        hint: "small red fruit",
+        picture: "assets/images/cherries-icon.png",
+        description: "a cherry",
+        level: "hard",
+        category: "fruit"       
+    }
+]
