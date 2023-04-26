233b51088c1369617da500abe9accd095f7154b4
Rob Killick <robkillick@me.com>
2023-04-26 16:06:58 +0000

    Tidy js file and add comments where missing

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     changeDifficulty(currentDifficulty);
 })
 
+// GLOBAL VARIABLES
+
 let topDisplay = document.getElementById("top-display");
 
 let playerMessage = document.getElementById("player-message");
@@ -61,17 +63,22 @@ let correctAnswer = "";
 let playerAnswer = [];
 
 let totalScore = 0;
+
 let scoreTarget = 2;
 
 let timeLeft = 60;
 
 let level = "";
 
+let startCountDown;
+
 let currentDifficulty = "easy";
 
 let challengeWords = [];
 
 let chosenWord;
+
+let levelGraphic;
 
 let usedWords =[];
 
@@ -126,6 +133,8 @@ const wordCollection = [
     }
 ]
 
+// FUNCTIONS
+
 /**
  * Toggles between home screen setting where player controls are hidden and
  * game screen where controlsa are visible and layout of the top box changes
@@ -140,8 +149,6 @@ function gameToggle(display) {
         topDisplay.innerHTML = scoreBox;
     }
 }
-
-let levelGraphic;
 
 /** 
  * Sets challenge difficulty level. Called by event listeners on settings page where the function will
@@ -184,7 +191,6 @@ function changeDifficulty(difficulty) {
     console.log("Challenge Words as game run" ,challengeWords);   
 }
 
-
 /**
  * The 5th and 6th answer and scramble boxes are hidden from display but remain in 
  * the DOM.  Grid set to a 4 column layout using the grid-tiles-4-class.
@@ -225,13 +231,18 @@ function hardDisplay() {
 }
 
 /**
- * Runs on click of "Go" button.  This makes sure that the default difficulty level is set to easy, shows
- * the score counter/ countdown timer, player controls and runs the playGame function.
+ * Runs on click of "Go" button.
+ * Resets the length of the challengeWords array to zero before it is populated 
+ * by the playGame function.  This makes sure that no words from other difficulty 
+ * settings appear in the next game. 
+ * Total score reset to zero.
+ * The default difficulty level is set according to the global currentDifficulty variable.
+ * Global timeLeft variable set to 60.
+ * Starts clock countdown.
+ * Runs playGame function
  */
-function runGame() {
-    // Clears challengeWords array before filling  
+function runGame() { 
     challengeWords.length = 0;
-    // usedWords.length = 0;
     totalScore = 0;
     changeDifficulty(currentDifficulty);
     gameToggle("game");
@@ -240,11 +251,9 @@ function runGame() {
     playGame();
 }
 
-let startCountDown;
-
 /**
- * Starts countdown from time specified in timeLeft variable.  Inserts timeLeft into the HTML and sends a message to
- * the player once time has run out for the challenge.
+ * Starts countdown from time specified in timeLeft variable.  
+ * Inserts timeLeft into the HTML and sends a message to the player once time has run out.
  */
 function countDown() {
     let timer = document.querySelector('#timer');
@@ -272,25 +281,25 @@ function countDown() {
     }
 }
 
+/**
+ * Sets interval for startCountDown variable.
+ */
 function startClock() {
     startCountDown = setInterval(countDown, 1000);
 }
 
+/**
+ * Clears Interval of the startCountDown variable.
+ */
 function stopClock() {
     clearInterval(startCountDown);     
 }
-
+/**
+ * Runs resetLetterBoxes and generateWord functions.
+ */
 function playGame() {
     resetLetterBoxes();
     generateWord();
-}
-
-function resetLetterBoxes() {
-    playerAnswer.length = 0;
-    clearAnswer();
-    buttonArray.forEach(function(currentLetter) {
-        currentLetter.disabled = false;
-    });
 }
 
 /**
@@ -298,7 +307,8 @@ function resetLetterBoxes() {
  * wordLetters array and scrambles these in a random order.
  * I used the following tutorial to help with coding this: https://www.youtube.com/watch?v=4-s3g_fU7Vg
  * The scrambled word is checked against the correct answer to make sure they are never the same.  If this
- * evaluates to false, the scramble boxes are populated with the scrambled letters.
+ * evaluates to false, the scramble boxes are populated with the scrambled letters. And the related
+ * image url is inserted into the html.
  * At the end of the function, the index of random word is stored in the variable chosenWord.  This is
  * used in the handleOldWords function to remove a word form the challengeWords once it has ben used, so
  * the same words do not keep coming up.
@@ -324,11 +334,22 @@ function generateWord() {
         displayMain.innerHTML = `<img src = "${pictureHint}">`;
         // Finds index of the random word within the challengeWords array.
         chosenWord = challengeWords.indexOf(randomWord);
-        console.log("Index of chosen word:" , chosenWord);
         // handleOldWords();
     }   
 }
 
+/**
+ * Sets the playerAnswer array to a length of zero in readiness for the next answer.
+ * clearAnswer function emptys the text from the Answer boxes.
+ * Scramble buttons are reactivated so the player can input a fresh answer.
+ */
+function resetLetterBoxes() {
+    playerAnswer.length = 0;
+    clearAnswer();
+    buttonArray.forEach(function(currentLetter) {
+        currentLetter.disabled = false;
+    });
+}
 
 /**
  * Removes the word that has just been displayed from the
@@ -359,7 +380,7 @@ function generateWord() {
     
 
 /**
- * Clears all answer boxes
+ * Clears all answer boxes of their letters
  */
 function clearAnswer() {
     for(let letter of answerLetters){
@@ -368,7 +389,7 @@ function clearAnswer() {
 }
 
 /**
- * Clears all scramble boxes
+ * Clears all scramble boxes of their letters.
  */
 function clearScramble(){
     for(let button of scrambleButtons){
@@ -376,7 +397,14 @@ function clearScramble(){
     }
 }
 
-// Add event listeners for all boxes containing a scrambled letter
+/**
+ * Add event listeners for all boxes containing a scrambled letter. 
+ * Clicking on each scrambled letter then pushes this to the playerAnswer 
+ * array. Also checks if an answer box is empty and if so poulates it with 
+ * the scramble letter that has been clicked.
+ * After each scramble button is clicked, it is disabled so the same letter 
+ * cannot be entered again.
+*/
 buttonArray.forEach(function(currentLetter) {
     currentLetter.addEventListener('click', function(event){
         if (playerAnswer.length < answerBoxes.length) {
@@ -400,11 +428,13 @@ buttonArray.forEach(function(currentLetter) {
 });
 
 /**
- * Turns the playerAnswer array into a string and compares this
- * against the correct answer.
+ * Turns the playerAnswer array into a string, converts the 
+ * correct answer to upper case letters and then compares the
+ * two against each other.  If they are equal, a point is added
+ * to the score counter and styling for the answer boxes change
+ * to give the player feedback.
  */
 function checkAnswer() {
-    // handleOldWords();
     let submittedAnswer = playerAnswer.join("");
     let correctUpperAnswer = correctAnswer.toUpperCase();
     if (submittedAnswer === correctUpperAnswer) {
@@ -414,8 +444,19 @@ function checkAnswer() {
     }
 }
 
-
-
+/**
+ * Checks total score against score target.  If equal: 
+ * The countdown clock is stopped. 
+ * Global currentDifficulty level updated according to the current value
+ * so the level of difficulty increases if a challeneg is completed.
+ * Message and associated image displayed based on which difficulty
+ * level has been completed.
+ * Answer and scramble boxes cleared.
+ * The run game button is made visible so the player can play again.
+ * Player controls are hidden.
+ * If the score target has not been reached, the playGame function runs which
+ * generates the next word.
+ */
 function checkScore() {
     if (totalScore === scoreTarget) {
         stopClock();
@@ -452,11 +493,41 @@ function checkScore() {
     } 
 }
 
+/**
+ * The global total Score variable is incremented by 1 and this value is
+ * inserted in the score box.
+ */
 function addPoint() {
     let score = document.getElementById("score");
     totalScore++;
     score.innerText = totalScore;
 }
+
+/**
+ * Function used by the run game event listener.
+ * I had to research and implement Event Bubbling for this, as the event
+ * listener was not not working when I dynamically reloaded the html for
+ * the button after successful completion of a challenge.
+ * I used and tweaked code from the following article to get this working:
+ * https://dev.to/akhil_001/adding-event-listeners-to-the-future-dom-elements-using-event-bubbling-3cp1
+ */
+function playButtonEventListener (selector, event, handler) {
+    let rootElement = document.querySelector('#display-main');
+    rootElement.addEventListener(event, function (evt) {
+            var targetElement = evt.target;
+            while (targetElement != null) {
+                if (targetElement.matches(selector)) {
+                    handler(evt);
+                    return;
+                }
+                targetElement = targetElement.parentElement;
+            }
+        },
+        true
+    );
+}
+
+// EVENT LISTENERS
 
 /**
  * Toggles the 'hidden' class to show and hide the instructions box.
@@ -466,14 +537,15 @@ function toggleInstructions() {
     topDisplay.classList.toggle('hidden');
 }
 
+/**
+ * Toggles the 'hidden' class to show and hide the settings box.
+ */
 function toggleSettings() {
     document.getElementById("settings").classList.toggle('hidden');
     topDisplay.classList.toggle('hidden'); 
 }
 
-
-
-// Event listener for submit answer button.  Runs checkAnswer function
+// Event listener for submit answer button.  Runs checkAnswer function.
 submitAnswer[0].addEventListener('click', checkAnswer);
 submitAnswer[0].addEventListener('click', checkScore);
 
@@ -497,54 +569,23 @@ hideInstructions.addEventListener('click', toggleInstructions);
 const hideSettings = document.getElementById("hide-settings");
 hideSettings.addEventListener('click', toggleSettings);
 
-// Event listener for easy difficulty settings button
+// Event listener for easy difficulty settings button.
 const selectEasy = document.getElementById("select-easy");
 selectEasy.addEventListener('click' , function() {
     currentDifficulty = "easy";   
 });
 
-// Event listener for easy difficulty settings button
+// Event listener for easy difficulty settings button.
 const selectMedium = document.getElementById("select-medium");
 selectMedium.addEventListener('click' , function() {
     currentDifficulty = "medium";    
 });
 
-// Event listener for easy difficulty settings button
+// Event listener for easy difficulty settings button.
 const selectHard = document.getElementById("select-hard");
 selectHard.addEventListener('click' , function() {
     currentDifficulty = "hard";
 });
 
-// const selectMedium = document.getElementById("select-medium");
-// selectEasy.addEventListener('click' , changeDifficulty("medium"));
-
-// const selectHard = document.getElementById("select-hard");
-// selectEasy.addEventListener('click' , changeDifficulty("hard"));
-
-/**
- * Event listener for play button which triggers the runGame function.
- * I had to research and implement Event Bubbling for this, as the event
- * listener was not not working when I dynamically reloaded the html for
- * the button after successful completion of a challenge.
- * I used and tweaked code from the following article to get this working:
- * https://dev.to/akhil_001/adding-event-listeners-to-the-future-dom-elements-using-event-bubbling-3cp1
- */
-
-function playButtonEventListener (selector, event, handler) {
-    let rootElement = document.querySelector('#display-main');
-    //since the root element is set to be body for our current dealings
-    rootElement.addEventListener(event, function (evt) {
-            var targetElement = evt.target;
-            while (targetElement != null) {
-                if (targetElement.matches(selector)) {
-                    handler(evt);
-                    return;
-                }
-                targetElement = targetElement.parentElement;
-            }
-        },
-        true
-    );
-}
-
+//Event listener for game run button
 playButtonEventListener('#play-game','click', runGame );
