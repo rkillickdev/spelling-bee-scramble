02d8a844e14c7a3bfe5721a98b69522d5082263d
Rob Killick <robkillick@me.com>
2023-04-27 15:49:59 +0000

    Update word collection array with alt descriptions for each word

--- assets/js/words.js
+++ assets/js/words.js
@@ -1,105 +1,104 @@
 const wordCollection = [
     {
         word: "pear",
-        hint: "soft juicy fruit",
         picture: "assets/images/pear-icon.png",
         description: "a green pear",
         level: "easy",
-        category: "fruit"
     },
     {
         word: "kiwi",
-        hint: "New Zealander",
         picture: "assets/images/kiwi-food-icon.png",
-        description: "a kiwi fruit",
-        level: "easy",
-        category: "fruit"     
+        description: "a green kiwi fruit",
+        level: "easy",    
     },
     {
         word: "cake",
         picture: "assets/images/cake-cup-color-icon.png",
+        description: "a cake with red frosting and a cherry on top",
         level: "easy",
     },
     {
         word: "star",
         picture: "assets/images/christmas-star-decoration-icon.png",
+        description: "a gold star",
         level: "easy",    
     },
     {
         word: "crab",
         picture: "assets/images/crab-color-icon.png",
+        description: "a red crab",
         level: "easy",  
     },
     {
         word: "gift",
         picture: "assets/images/gift-present-color-icon.png",
+        description: "a yellow and red wrapped gift",
         level: "easy", 
     },
     {
         word: "flag",
         picture: "assets/images/flag-green-icon.png",
+        description: "a green flag",
         level: "easy", 
     },
     {
         word: "bull",
         picture: "assets/images/buffalo-head-color-icon.png",
+        description: "a bull with horns",
         level: "easy",
     },
     {
         word: "tree",
         picture: "assets/images/green-tree-icon.png",
+        description: "a green tree",
         level: "easy",   
     },
     {
         word: "doll",
         picture: "assets/images/doll-icon.png",
+        description: "a cartoon doll with pink dress",
         level: "easy",       
     },
     {
         word: "leaf",
         picture: "assets/images/leaf-green-color-icon.png",
+        description: "a green leaf",
         level: "easy",       
     },
     {
         word: "teddy",
         picture: "assets/images/teddy-bear-brown-icon.png",
+        description: "a brown teddy bear",
         level: "medium", 
     },
     {
         word: "sweet",
         picture: "assets/images/christmas-candy-sweet-icon.png",
+        description: "a red and yellow wrapped sweet",
         level: "medium",
     },
     {
         word: "peach",
-        hint: "a furry fruit",
         picture: "assets/images/peach-fruit-icon.png",
-        description: "a peach",
-        level: "medium",
-        category: "fruit"     
+        description: "a peach with green leaf",
+        level: "medium",    
     },
     {
-        word: "APPLE",
-        hint: "crunchy fruit",
+        word: "apple",
         picture: "assets/images/fresh-apple-icon.png",
-        description: "an apple",
-        level: "medium",
-        category: "fruit" 
+        description: "a red apple with green leaf",
+        level: "medium", 
     },
     {
         word: "carrot",
-        hint: "orange vegetable",
         picture: "assets/images/carrot-icon.png",
-        description: "a carrot",
-        level: "hard",
-        category: "vegetable" 
+        description: "a carrot with green top",
+        level: "hard", 
     },
     {
         word: "cherry",
-        hint: "small red fruit",
         picture: "assets/images/cherries-icon.png",
-        description: "a cherry",
-        level: "hard",
-        category: "fruit"       
+        description: "some red cherries on a red stalk",
+        level: "hard",     
     }
 ];
