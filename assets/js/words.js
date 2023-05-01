6bb4b996f62c2f09274378aab09eda3609a27f37
Rob Killick <robkillick@me.com>
2023-05-01 18:54:46 +0000

    Add more objects to the word collection array

--- assets/js/words.js
+++ assets/js/words.js
@@ -1,104 +1,170 @@
 const wordCollection = [
     {
-        word: "pear",
-        picture: "assets/images/pear-icon.png",
-        description: "a green pear",
-        level: "easy",
+        word: "corn",
+        picture: "assets/images/corn-icon.png",
+        description: "a corn on the cob",
+        level: "easy"
+    },
+    {   word: "fish",
+        picture: "assets/images/fish-color-icon.png",
+        description: "an orange fish",
+        level: "easy"
+
     },
     {
         word: "kiwi",
         picture: "assets/images/kiwi-food-icon.png",
         description: "a green kiwi fruit",
-        level: "easy",    
+        level: "easy"    
     },
     {
         word: "cake",
         picture: "assets/images/cake-cup-color-icon.png",
         description: "a cake with red frosting and a cherry on top",
-        level: "easy",
+        level: "easy"
     },
     {
         word: "star",
         picture: "assets/images/christmas-star-decoration-icon.png",
         description: "a gold star",
-        level: "easy",    
+        level: "easy"    
     },
     {
         word: "crab",
         picture: "assets/images/crab-color-icon.png",
         description: "a red crab",
-        level: "easy",  
+        level: "easy"  
     },
     {
         word: "gift",
         picture: "assets/images/gift-present-color-icon.png",
         description: "a yellow and red wrapped gift",
-        level: "easy", 
+        level: "easy" 
     },
     {
         word: "flag",
         picture: "assets/images/flag-green-icon.png",
         description: "a green flag",
-        level: "easy", 
+        level: "easy"
     },
     {
         word: "bull",
         picture: "assets/images/buffalo-head-color-icon.png",
         description: "a bull with horns",
-        level: "easy",
+        level: "easy"
     },
     {
         word: "tree",
         picture: "assets/images/green-tree-icon.png",
         description: "a green tree",
-        level: "easy",   
+        level: "easy"   
     },
     {
         word: "doll",
         picture: "assets/images/doll-icon.png",
         description: "a cartoon doll with pink dress",
-        level: "easy",       
+        level: "easy"       
     },
     {
         word: "leaf",
         picture: "assets/images/leaf-green-color-icon.png",
         description: "a green leaf",
-        level: "easy",       
+        level: "easy"       
     },
     {
         word: "teddy",
         picture: "assets/images/teddy-bear-brown-icon.png",
         description: "a brown teddy bear",
-        level: "medium", 
+        level: "medium" 
     },
     {
         word: "sweet",
         picture: "assets/images/christmas-candy-sweet-icon.png",
         description: "a red and yellow wrapped sweet",
-        level: "medium",
+        level: "medium"
     },
     {
         word: "peach",
         picture: "assets/images/peach-fruit-icon.png",
         description: "a peach with green leaf",
-        level: "medium",    
+        level: "medium"    
     },
+    {   word: "earth",
+        picture: "assets/images/earth-icon.png",
+        description: "The planet earth from above",
+        level: "medium"   
+    },
+    {   word: "lolly",
+        picture: "assets/images/ice-cream-stick-icon.png",
+        description: "A pink ice lolly",
+        level: "medium"   
+    },
+    {   word: "lemon",
+        picture: "assets/images/lemon-icon.png",
+        description: "A lemon",
+        level: "medium"
+    },
+    {   word: "heart",
+        picture: "assets/images/red-heart-icon.png",
+        description: "A red heart",
+        level: "medium"   
+    },
+    {   word: "melon",
+        picture: "assets/images/watermelon-fruit-icon.png",
+        description: "A slice of melon",
+        level: "medium"
+    },   
     {
         word: "apple",
         picture: "assets/images/fresh-apple-icon.png",
         description: "a red apple with green leaf",
-        level: "medium", 
+        level: "medium" 
+    },
+    {   word: "toast",
+        picture: "assets/images/toast-color-icon.png",
+        description: "a slice of toast",
+        level: "medium"
     },
     {
         word: "carrot",
         picture: "assets/images/carrot-icon.png",
         description: "a carrot with green top",
-        level: "hard", 
+        level: "hard" 
     },
     {
         word: "cherry",
         picture: "assets/images/cherries-icon.png",
         description: "some red cherries on a red stalk",
-        level: "hard",     
-    }
+        level: "hard"     
+    },
+    {   word: "tomato",
+        picture: "assets/images/tomato-icon.png",
+        description: "a red tomato with green stalk",
+        level: "hard"  
+    },
+    {   word: "orange",
+        picture: "assets/images/orange-icon.png",
+        description: "an orange",
+        level: "hard"
+    },
+    {   word: "cactus",
+        picture: "assets/images/cactus-plant-icon.png",
+        description: "a green cactus plant",
+        level: "hard"
+    },
+    {   word: "banana",
+        picture: "assets/images/banana-icon.png",
+        description: "a peeled banana",
+        level: "hard"
+    },
+    {   word: "cookie",
+        picture: "assets/images/cookie-icon.png",
+        description: "a cookie",
+        level: "hard"
+    },
+    {   word: "sponge",
+        picture: "assets/images/sponge-icon.png",
+        description: "a yellow sponge",
+        level: "hard"
+    },
 ];
